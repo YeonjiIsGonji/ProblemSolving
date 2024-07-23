cabe2f89ff468702be9f02a144506267612985e7
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-07-23 09:49:46 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,22 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tk = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(tk.nextToken());
-        int M = Integer.parseInt(tk.nextToken());
+        int X = Integer.parseInt(tk.nextToken());
+        int Y = Integer.parseInt(tk.nextToken());
 
         Queue<int[]> queue = new LinkedList<>();
-        int[][] graph = new int[N][M];
-        int[][] visited = new int[N][M];
-        int count = 0;
+        int[][] graph = new int[X][Y];
+        int[][] visited = new int[X][Y];
 
         // 그래프 만들기
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < X; i++) {
             String input = br.readLine();
             char[] arr = input.toCharArray();
             for (int j = 0; j < arr.length; j++) {
@@ -24,41 +26,32 @@ public class Main {
             }
         }
 
+        // 방향 벡터 (상, 하, 좌, 우)
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+
         // 그래프 탐색
         queue.add(new int[]{0, 0});
         visited[0][0] = 1;
 
         while (!queue.isEmpty()) {
             int[] currNode = queue.poll();
-            int currN = currNode[0];
-            int currM = currNode[1];
-            
-            // 현재 노드에서 위로 이동
-            if (currM >= 1 && visited[currN][currM - 1] == 0 && graph[currN][currM - 1] == 1) {
-                queue.add(new int[]{currN, currM - 1});
-                visited[currN][currM - 1] = visited[currN][currM] + 1;
-            }
-            
-            // 현재 노드에서 아래로 이동
-            if (currM <= M - 2 && visited[currN][currM + 1] == 0 && graph[currN][currM + 1] == 1) {
-                queue.add(new int[]{currN, currM + 1});
-                visited[currN][currM + 1] = visited[currN][currM] + 1;
-            }
-        
-            // 현재 노드에서 왼쪽으로 이동
-            if (currN >= 1 && visited[currN - 1][currM] == 0 && graph[currN - 1][currM] == 1) {
-                queue.add(new int[]{currN - 1, currM});
-                visited[currN - 1][currM] = visited[currN][currM] + 1;
-            }
-    
-            // 현재 노드에서 오른쪽으로 이동
-            if (currN <= N - 2 && visited[currN + 1][currM] == 0 && graph[currN + 1][currM] == 1) {
-                queue.add(new int[]{currN + 1, currM});
-                visited[currN + 1][currM] = visited[currN][currM] + 1;
-            }
+            int currX = currNode[0];
+            int currY = currNode[1];
 
+            for (int i = 0; i < 4; i++) {
+                int nextX = currX + dx[i];
+                int nextY = currY + dy[i];
+
+                // 유효한 범위 내에 있으며, 아직 방문하지 않았고 이동 가능한 경우
+                if (nextX >= 0 && nextX < X && nextY >= 0 && nextY < Y && visited[nextX][nextY] == 0 && graph[nextX][nextY] == 1) {
+                    queue.add(new int[]{nextX, nextY});
+                    visited[nextX][nextY] = visited[currX][currY] + 1;
+                }
+            }
         }
-        System.out.println(visited[N - 1][M - 1]);
+        // (N-1, M-1) 위치까지의 최소 거리를 출력
+        System.out.println(visited[X - 1][Y - 1]);
     }
-
 }
