eddc18e88807b1a908786af55a99025f4034c21d
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-12-10 00:28:45 +0900

    [Silver I] Title: 안전 영역, Time: 296 ms, Memory: 38256 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,7 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Stream;
 
+/**
+ * 1. N, graph 입력 받기 - 최소 높이, 최대 높이 구하기
+ * 2. 최소 높이 ~ (최대 높이 - 1) 까지 각 case별 BFS 구하기
+ *  2-1. 그래프 전체 순회하며 BFS로 안전 영역 개수 구하기
+ * 3. 안전 영역 최대 값 출력
+ */
 public class Main {
     static int N;
 
@@ -12,20 +19,23 @@ public class Main {
         int[][] graph = new int[N][N];
         int minH = Integer.MAX_VALUE;
         int maxH = 0;
-
         for (int i = 0; i < N; i++) {
-            String[] height = br.readLine().split(" ");
+            String input = br.readLine();
+            String[] height = input.split(" ");
             for (int j = 0; j < N; j++) {
                 graph[i][j] = Integer.parseInt(height[j]);
                 minH = Math.min(minH, graph[i][j]);
                 maxH = Math.max(maxH, graph[i][j]);
             }
         }
-
-        int maxSafeZone = 0;
-        for (int i = minH - 1; i <= maxH; i++) { // 비가 오지 않는 경우도 포함
-            maxSafeZone = Math.max(maxSafeZone, BFS(i, graph));
+        
+        int maxSafeZone = 1; // 비가 오지 않을 경우 최소 1개
+        
+        for (int i = minH; i < maxH; i++) {
+            int safeZone = BFS(i, graph);
+            maxSafeZone = Math.max(maxSafeZone, safeZone);
         }
+
         System.out.println(maxSafeZone);
     }
 
@@ -40,15 +50,17 @@ public class Main {
             for (int j = 0; j < N; j++) {
                 if (graph[i][j] > height && !visit[i][j]) {
                     queue.offer(new int[]{i, j});
-                    visit[i][j] = true; // 방문 처리
+                    visit[i][j] = true;
                     while (!queue.isEmpty()) {
                         int[] cur = queue.poll();
                         for (int k = 0; k < 4; k++) {
                             int nextX = cur[0] + dx[k];
                             int nextY = cur[1] + dy[k];
-                            if (isValid(nextX, nextY, height, graph, visit)) {
-                                queue.offer(new int[]{nextX, nextY});
-                                visit[nextX][nextY] = true; // 방문 처리
+                            if (isValid(nextX, nextY)) {
+                                if (graph[nextX][nextY] > height && !visit[nextX][nextY]) {
+                                    queue.offer(new int[]{nextX, nextY});
+                                    visit[nextX][nextY] = true;
+                                }
                             }
                         }
                     }
@@ -56,10 +68,11 @@ public class Main {
                 }
             }
         }
+
         return count;
     }
 
-    private static boolean isValid(int x, int y, int height, int[][] graph, boolean[][] visit) {
-        return 0 <= x && x < N && 0 <= y && y < N && graph[x][y] > height && !visit[x][y];
+    private static boolean isValid(int x, int y) {
+        return 0 <= x && x < N && 0 <= y && y < N;
     }
 }
