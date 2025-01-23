9e2f263687b9b55739a1ecca64ad3cd3e3112813
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2025-01-23 23:03:33 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long result = fibonacci(N);
+        System.out.println(result);
+    }
+
+    public static long fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
